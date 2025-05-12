35813116d1517ebd462099a09b3f3f72ed556a17
Daniel Kirk <imdanielkirk@gmail.com>
2025-05-11 22:45:51 -0700

    benchmarking memory allocations

--- app.go
+++ app.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Product struct {
 	price int
 	name  string
@@ -26,16 +28,38 @@ func printStats(products []Product) {
 	println(cost)
 	println(totalItems)
 	println(products[totalItems-1].name)
+	println(len(products), cap(products))
 
 }
 func main() {
 
-	shoppingList := []Product{{12, "bacon"}, {30, "eggs"}, {4, "cheese"}}
+	shoppingList := []Product{{12, "bacon"}, {30, "eggs"}, {4, "cheese"}, {5, "meal"}}
+	newList := []Product{}
 
-	printStats(shoppingList)
+	//omits the first index item and returns the rest
+	printStats(shoppingList[1:])
 
-	shoppingList = append(shoppingList, Product{5, "meal"})
+	newList = append(newList, shoppingList...)
 
-	printStats(shoppingList)
+	//omits the first two indexed items and returns the rest
+	printStats(newList)
+	newList = append(newList, shoppingList...)
+	//returns the entire array
+	printStats(newList)
+	newList = append(newList, shoppingList...)
+	//returns the entire array without explicitly stating it
+	printStats(newList)
+
+	//since I know the array will be 4 I can preallocate the slice.
+	preAllocatedSlice := make([]Product, 0, len(shoppingList)*4)
+	preAllocatedSlice = append(preAllocatedSlice, shoppingList...)
+	fmt.Printf("d: %p, len: %d, cap: %d\n", preAllocatedSlice, len(preAllocatedSlice), cap(preAllocatedSlice))
+
+	preAllocatedSlice = append(preAllocatedSlice, shoppingList...)
+	fmt.Printf("d: %p, len: %d, cap: %d\n", preAllocatedSlice, len(preAllocatedSlice), cap(preAllocatedSlice))
+	preAllocatedSlice = append(preAllocatedSlice, shoppingList...)
+	fmt.Printf("d: %p, len: %d, cap: %d\n", preAllocatedSlice, len(preAllocatedSlice), cap(preAllocatedSlice))
+	preAllocatedSlice = append(preAllocatedSlice, shoppingList...)
+	fmt.Printf("d: %p, len: %d, cap: %d\n", preAllocatedSlice, len(preAllocatedSlice), cap(preAllocatedSlice))
 
 }
