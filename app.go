33c03abe4299e621888a1a6c535cb7a5849b07ca
Daniel Kirk <imdanielkirk@gmail.com>
2025-05-12 22:12:48 -0700

    rangin it

--- app.go
+++ app.go
@@ -2,27 +2,13 @@ package main
 
 import "fmt"
 
-type Part string
-
-func printParts(parts []Part) {
-	var partsList string = "these parts"
-	for i := 0; i < len(parts); i++ {
-		partsList += ` ` + string(parts[i])
-	}
-
-	fmt.Println(partsList)
-}
-
 func main() {
-	assemblyLine := []Part{"wrench", "screwdriver", "hammer"}
+	slice := []string{"hey", "how's", "it", "going"}
 
-	printParts(assemblyLine)
-
-	assemblyLine = append(assemblyLine, "nailgun", "pipe")
-
-	printParts(assemblyLine)
-
-	assemblyLine = assemblyLine[3:]
-
-	printParts(assemblyLine)
+	for i, element := range slice {
+		fmt.Println(i, element)
+		for _, ch := range element {
+			fmt.Printf("      %q\n", ch)
+		}
+	}
 }
